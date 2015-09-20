e3e2b4c735a24ad56144e7296b83a180e8804ba5
ryosuke0032 <ryosuke0032@gmail.com>
2015-09-20 03:19:22 +0000

    kadai 1

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -12,7 +12,9 @@ class OwnershipsController < ApplicationController
     if @item.new_record?
       begin
         # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
-        response = {}
+        response = Amazon::Ecs.item_lookup(params[:asin] , 
+                                  :response_group => 'Medium' , 
+                                  :country => 'jp')
       rescue Amazon::RequestError => e
         return render :js => "alert('#{e.message}')"
       end
@@ -28,6 +30,14 @@ class OwnershipsController < ApplicationController
     end
 
     # TODO ユーザにwant or haveを設定する
+    if params[:type] == "Have"
+      current_user.have(@item)
+    end
+    
+    if params[:type] == "Want"
+      current_user.want(@item)
+    end
+    
     # params[:type]の値ににHaveボタンが押された時には「Have」,
     # Wantボタンがされた時には「Want」が設定されています。
     
@@ -40,6 +50,14 @@ class OwnershipsController < ApplicationController
     # TODO 紐付けの解除。 
     # params[:type]の値ににHavedボタンが押された時には「Have」,
     # Wantedボタンがされた時には「Want」が設定されています。
+    if params[:type] == "Have"
+      current_user.unhave(@item)
+    end
+    
+    if params[:type] == "Want"
+      current_user.unwant(@item)
+    end
+    
 
   end
 end
