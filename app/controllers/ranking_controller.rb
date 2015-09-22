d9b7c2bf02bb4f26e42142b1c515c6b8ff01cc04
ryosuke0032 <ryosuke0032@gmail.com>
2015-09-22 13:47:21 +0000

    saigono kadai ikkaime

---
+++ app/controllers/ranking_controller.rb
@@ -0,0 +1,13 @@
+class RankingController < ApplicationController
+
+    def have
+        @ids = Have.group(:item_id).order("count_item_id DESC").limit(10).count(:item_id).keys
+        @items = Item.find(@ids).sort_by{|o| @ids.index(o.id)}
+    end
+    
+    def want
+        @ids = Want.group(:item_id).order("count_item_id DESC").limit(10).count(:item_id).keys
+        @items = Item.find(@ids).sort_by{|o| @ids.index(o.id)}
+    end
+        
+end
